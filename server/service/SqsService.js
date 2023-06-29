7769b92e416a7e3d903ab288f4b78d99f3d5da84
manu <manukarnikasud@gmail.com>
2023-06-29 09:45:34 +0530

    added serverless

--- server/service/SqsService.js
+++ server/service/SqsService.js
@@ -5,8 +5,8 @@ const publishMessage = async (data)=>{
         const sqs = new AWS.SQS({
             apiVersion: '2012-11-05',
             region: process.env.REGION,
-            accessKeyId:  process.env.ACCESS_KEY_ID,
-            secretAccessKey: process.env.SECRET_ACCESS_KEY,
+            // accessKeyId:  process.env.ACCESS_KEY_ID,
+            // secretAccessKey: process.env.SECRET_ACCESS_KEY,
            });
            const url=`https://sqs.ap-south-1.amazonaws.com/${process.env.ACCOUNT_ID}/message-processor`;
            const payload = data;
---
+++ serverless/handler.js
@@ -0,0 +1,13 @@
+
+
+const handler = async (event, context) => {
+    try{
+        const eventRecord = event.Records?.[0];
+        const message = eventRecord?.body;
+        console.log("Message Received from SQS",message)
+    }catch(err){
+        console.log("Error in handler:",error);
+    }
+}
+
+module.exports = handler;
