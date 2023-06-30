84e9d85b410f10b94d7f6908909b4f4958e58f91
ubuntu <manukarnikasud@gmail.com>
2023-06-30 15:11:22 +0530

    added serverless

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,13 +1,11 @@
 
 
-const handler = async (event, context) => {
+exports.handler = async (event, context) => {
     try{
         const eventRecord = event.Records?.[0];
         const message = eventRecord?.body;
         console.log("Message Received from SQS",message)
     }catch(err){
-        console.log("Error in handler:",error);
+        console.log("Error in handler:",err);
     }
 }
-
-module.exports = handler;
